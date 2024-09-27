cd53b74d2e6b96fd7ce416ca93c3cc7e07c440c6
HamzaShabbir07 <hamxashabir007@gmail.com>
2024-09-27 08:44:04 +0500

    Connection Build

---
+++ BulkyWebApp/Models/Catergory.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebApp.Models
+{
+    public class Catergory
+    {
+        [Key]
+        public  int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public int Display_Order { get; set; }
+    }
+}
