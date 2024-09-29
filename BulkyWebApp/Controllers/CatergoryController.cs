02cf3bc46301fbb571aaf7e683256adad94d253f
HamzaShabbir07 <hamxashabir007@gmail.com>
2024-09-29 10:17:39 +0500

    Add Catergory in Navbar

---
+++ BulkyWebApp/Controllers/CatergoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWebApp.Controllers
+{
+    public class CatergoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
