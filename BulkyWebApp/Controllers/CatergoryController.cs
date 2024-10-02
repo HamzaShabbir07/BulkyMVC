48f0da50d2bbe77b047b1a8732bdcaeb17b69691
HamzaShabbir07 <hamxashabir007@gmail.com>
2024-10-02 09:19:42 +0500

    Post data to DB and add validation

--- BulkyWebApp/Controllers/CatergoryController.cs
+++ BulkyWebApp/Controllers/CatergoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWebApp.Data;
 using BulkyWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BulkyWebApp.Controllers
 {
@@ -15,6 +16,29 @@ namespace BulkyWebApp.Controllers
         {
             List<Catergory> objcatergorylist = _db.Catergories.ToList();
             return View(objcatergorylist);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Catergory obj)
+        {
+            if(obj.Name == obj.Display_Order.ToString())
+            {
+                ModelState.AddModelError("name", "Cannot Same value");
+                ModelState.AddModelError( "display_order", "Cannot Same value");
+            }
+            if (ModelState.IsValid)
+            {
+            _db.Catergories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            }
+            return View();
+            
+
         }
     }
 }
--- BulkyWebApp/Models/Catergory.cs
+++ BulkyWebApp/Models/Catergory.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWebApp.Models
 {
@@ -8,7 +9,12 @@ namespace BulkyWebApp.Models
         public  int ID { get; set; }
 
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Catergory Name")]
         public string Name { get; set; }
+
+        [Range(1,100, ErrorMessage ="This not Valid Number")]
+        [DisplayName("Display Order")]
         public int Display_Order { get; set; }
     }
 }
